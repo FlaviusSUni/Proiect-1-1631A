d83cce4b510320b7df35c00fb8578929aa9e1de6
FlaviusSUni <stasac.flaviusgabriel@student.uoradea.ro>
2025-11-13 20:52:19 +0200

    theme selector

---
+++ frontend/src/App.jsx
@@ -0,0 +1,22 @@
+import Navbar from "./components/Navbar/Navbar";
+import HomePage from "./pages/HomePage/HomePage";
+import ProductPage from "./pages/ProductPage/ProductPage";
+
+import { Routes, Route } from "react-router-dom";
+import { useThemeStore } from "./store/useThemeStore";
+
+function App() {
+  const {theme} = useThemeStore();
+  return (
+    <div className="min-h-screen bg-base-200 transition-colors duration-300" data-theme={theme}>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/product/123" element={<ProductPage />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
+
---
+++ frontend/src/pages/HomePage/HomePage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function HomePage() {
+  return (
+    <div>
+      <h1 className="text-4xl font-bold text-green-600">Home Page</h1>
+    </div>
+  )
+}
+
+export default HomePage
